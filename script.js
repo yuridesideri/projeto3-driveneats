a4452d7f2e918009c080dac96af7bc7c6e83eaba
Yuri <yuridesideri12@gmail.com>
2022-08-11 19:11:06 -0300

    Final Javascript Button Changes

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 const selectionElements = [document.getElementsByClassName("food-items-1"),document.getElementsByClassName("food-items-2"), document.getElementsByClassName("food-items-3")];
+function requestFood()
+{
+    let requestButton = document.getElementsById("request");
+    let check = 0;
+    for (let i = 0; i < selectionElements[0].length; i++)
+    {
+        if (selectionElements[0][i].classList.contains("selected"))
+        {
+            check++;
+            break;
+            
+        }
+    }
+    for (let i = 0; i < selectionElements[1].length; i++)
+    {
+        if (selectionElements[0][i].classList.contains("selected"))
+        {
+            check++;
+            break;
+        }
+    }
+    for (let i = 0; i < selectionElements[2].length; i++)
+    {
+        if (selectionElements[0][i].classList.contains("selected"))
+        {
+            check++;
+            break;
+        }
+    }
+    if (check === 3)
+    {
+
+        requestButton.innerHTML = "Fechar pedido";
+        requestButton.getElementById("request").style.backgroundColor =  "#32B72F;";
+        requestButton.getElementById("request").style.border = "solid #000000 1px;";
+    }
+    else 
+    {
+        requestButton.innerHTML = "Selecione os 3 itens para fechar o pedido";
+        requestButton.getElementById("request").style.backgroundColor =  "#CBCBCB;";
+        requestButton.getElementById("request").style.border = "none;";	
+    }
+}
+
 
 function resetSelection(p)
 {
@@ -28,14 +72,11 @@ function selectElement(element)
                     element.classList.add("selected");
                     selectionElements[i][j].style.boxShadow = "0px 0px 10px -4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";    
                     tmp = false;
-                    requestFood();
                 }
+                console.log("alguma coisa");
+                requestFood();
+                return;
             }
         }
     }
-}
-
-function requestFood()
-{
-    
 }
